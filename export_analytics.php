b04e53ed7b8f79e94236f11762e4e2da3558a853
EBethke <erik.k.bethke@gmail.com>
2018-06-11 14:41:17 -0400

    date-field
    add date field instead of "live visitors" in pursuit of appending instead of updating

--- export_analytics.php
+++ export_analytics.php
@@ -22,7 +22,7 @@ if ($statement->rowCount() > 0) {
   $delimiter = ",";
 
   // Set column headers for the csv file.
-  $fields = array('Agency', 'Mission area', 'Division', 'Program', 'Program Category', 'URL', 'Domain', 'Description', 'Program Type', 'Cost', 'Reviewed By', "Visits", 'Page Views', 'Prior Live Visitors', $row['date'], 'Updated Date');
+  $fields = array('Agency', 'Mission area', 'Division', 'Program', 'Program Category', 'URL', 'Domain', 'Description', 'Program Type', 'Cost', 'Reviewed By', "Visits", 'Page Views', 'Prior Live Visitors', date("Y-m-d H:i:s"), 'Updated Date');
   fputcsv($file, $fields, $delimiter);
 
   // Output each row of the data, format line as csv and write to file pointer.
